a7128b7fb4ccef75a80d8d9367f8441a770d7a18
JUAN <joahn.cardenas@epn.edu.ec>
2024-07-27 13:31:12 -0500

    si

---
+++ src/Conexion.java
@@ -0,0 +1,23 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseConnection {
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+
+    public static void connect() {
+        mongoClient = MongoClients.create("mongodb://localhost:27017");
+        database = mongoClient.getDatabase("CellTechHubDB");
+    }
+
+    public static MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public static void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
+}
